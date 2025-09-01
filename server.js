629ee16b099b58746c2b73d973fabb155e11b5f6
dharaneeshponnuvel <dharaneeshp56@gmail.com>
2025-09-01 21:28:22 +0530

    Initial commit: Upload uber-backend project

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "uber-frontend-gamma.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
   }
 });
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "uber-frontend-gamma.vercel.app",
   credentials: true
 }));
 app.use(express.json());
